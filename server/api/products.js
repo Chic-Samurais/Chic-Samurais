99fbbbe6fe65d5ad3b9219fcd005f1c242f755c6
RWahrman <rebecca.wahrman@gmail.com>
2020-04-22 17:29:24 -0400

    feat: product routes no security middleware

--- server/api/products.js
+++ server/api/products.js
@@ -1,11 +1,13 @@
 const router = require('express').Router()
 const {Product} = require('../db/models')
+// const {Op} = require('sequelize')
+
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const product = await Product.findAll()
-    res.json(product)
+    const products = await Product.findAll()
+    res.json(products)
   } catch (err) {
     next(err)
   }
@@ -20,4 +22,48 @@ router.get('/:productId', async (req, res, next) => {
   }
 })
 
-//post, put, and delete as admin????
+//ADMIN  ROUTES
+router.post('/', async (req, res, next) => {
+  try {
+    const newProduct = await Product.create(req.body)
+    res.json(newProduct) // res.redirecct?? what do we want rendered here/sent to redux store???
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId)
+    const updated = await product.update(req.body)
+    res.json(updated) // what do we want sent? possibility of redirect?
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId)
+    await product.destroy()
+    res.sendStatus(204)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//GET ROUTES FOR ADMIN ALL
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const product = await Product.findAll({
+//       where: {
+//         quantity: {
+//           [Op.gt]: 0,
+//         },
+//       },
+//     })
+//     res.json(product)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
--- server/api/users.js
+++ server/api/users.js
@@ -1,11 +1,6 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
-
-// add rudiments of security
-// look into middleware
-
-// GET /api/users
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -13,9 +8,19 @@ router.get('/', async (req, res, next) => {
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
       attributes: ['id', 'email']
+      //For code review: do we want to exclude passwords to admins too if we're planning on protecting this route?
     })
     res.json(users)
   } catch (err) {
     next(err)
   }
 })
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId)
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+//do we want users to have access to their single route and if so, how do we protect it (using sessions?) and is there anything we should exclude?
