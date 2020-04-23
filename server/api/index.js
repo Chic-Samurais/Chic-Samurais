37775e8dfdf69d6ae5b30cf4b45d1bb43241dcb9
Chelsea Church <chelsea.ae.church@gmail.com>
2020-04-23 10:39:11 -0400

    feat(seedfile): working, but only with reduced user model

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/posters', require('./posters'))
+router.use('/products', require('./products'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
