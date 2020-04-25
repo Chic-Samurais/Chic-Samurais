ad2e866869cfae6ab6581924b890eb781a08723a
Felicia Heiney <bhfelicia@gmail.com>
2020-04-25 11:12:52 -0400

    fix: protects user routes and handles errors

--- server/api/users.js
+++ server/api/users.js
@@ -2,16 +2,19 @@ const router = require('express').Router()
 const {User, Order} = require('../db/models')
 module.exports = router
 
-const adminsOnly = function(req, res, next) {
-  if (!req.user || !req.user.isAdmin) {
-    res.redirect('/')
-  }
-  next()
-}
+// const adminsOnly = function(req, res, next) {
+//     if (!req.user || !req.user.isAdmin) {
+//       res.redirect('/')
+//     } else {
+//       next()
+//     }
+// }
 
-router.get('/', adminsOnly, async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    console.log('request user is: ', req.user)
+    if (!req.user || !req.user.isAdmin) {
+      res.redirect('/')
+    }
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
@@ -26,7 +29,7 @@ router.get('/', adminsOnly, async (req, res, next) => {
 })
 
 //do we want users to have access to their single route and if so, how do we protect it (using sessions?) and is there anything we should exclude?
-router.get('/:userId', adminsOnly, async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.userId, {
       include: [
@@ -64,8 +67,11 @@ router.put('/:userId', async (req, res, next) => {
   }
 })
 
-router.delete('/:userId', adminsOnly, async (req, res, next) => {
+router.delete('/:userId', async (req, res, next) => {
   try {
+    if (!req.user || !req.user.isAdmin) {
+      res.redirect('/')
+    }
     const user = await User.findByPk(req.params.userId)
     await user.destroy()
     res.sendStatus(204)
