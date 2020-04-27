262e08e88c64d5b39d5ffdfd0a1055e65af0f413
Chelsea Church <chelsea.ae.church@gmail.com>
2020-04-27 14:21:48 -0400

    feat(client): re-committing before merge to avoid errors

--- client/store/index.js
+++ client/store/index.js
@@ -4,7 +4,7 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import product from './product'
-import cart from './cart';
+import cart from './cart'
 
 const reducer = combineReducers({user, product, cart})
 const middleware = composeWithDevTools(
@@ -13,6 +13,6 @@ const middleware = composeWithDevTools(
 const store = createStore(reducer, middleware)
 
 export default store
-export * from './user';
-export * from './product';
-export * from './cart';
+export * from './user'
+export * from './product'
+export * from './cart'
