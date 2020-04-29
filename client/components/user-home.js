e42f28dd8a0533ca3deab0251ffadb2b8324ba85
RWahrman <rebecca.wahrman@gmail.com>
2020-04-29 03:41:49 -0400

    additional stylistic things

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -13,14 +13,29 @@ export const UserHome = props => {
     <div>
       {isAdmin ? (
         <div>
-          <h3>Welcome, {email}</h3>
+          <h3>Welcome, {email}! </h3>
+          <h4>
+            <em>
+              Thank you for helping to make this site as beautiful as one of our
+              many *mock* masterpieces!
+            </em>
+          </h4>
           <Link to="/admin/products">Edit Products</Link>
+          <br />
           <br />
           <Link to="/admin/users">View Users</Link>
         </div>
       ) : (
         <div>
-          <h3>Welcome, {email}</h3>
+          <h3>Welcome back, {email}!</h3>
+          <h4>
+            <i>
+              May your shopping experience be as fine as the art in our
+              inventory . . .
+            </i>
+          </h4>
+          <br />
+          <h3>🖼 Previously Procured Posters 🖼</h3>
         </div>
       )}
     </div>
